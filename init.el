836d41d6649555c44bc55b1ae34fec9e404a2c6f
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2017-01-13 13:35:24 +0100

    company and jedi seems to work now

--- init.el
+++ init.el
@@ -1,17 +1,23 @@
 (require 'cl)
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("Melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/"))
 
 (let* ((packages
 	'(auto-compile
+	  company
+	  company-jedi			; remember to clone jedi and replace the automatically installed version in .emacs.d/.python-environments
+	  company-quickhelp
 	  fill-column-indicator
+	  flycheck
 	  idle-require
 	  helm
-	  jedi
+	  helm-company
 	  magit
 	  markdown-mode
 	  monokai-theme
+	  pyvenv
 	  zenburn-theme))
        (packages (remove-if 'package-installed-p packages)))
   (when packages
@@ -22,7 +28,8 @@
 
 (dolist (feature
 	 '(auto-compile
-	   jedi))
+	   ;;jedi
+	   ))
   (idle-require feature))
 
 (setq idle-require-idle-delay 5)
@@ -55,6 +62,7 @@
 ;; Enable useful information minor modes.
 (column-number-mode 1)
 (show-paren-mode 1)
+(global-company-mode 1)
 (recentf-mode 1)
 (setq-default recent-save-file "~/.emacs.d/recentf")
 
@@ -63,19 +71,6 @@
       `((".*" . ,emacs-autosave-directory))
       auto-save-file-name-transforms
       `((".*" ,emacs-autosave-directory t)))
-
-
-;; Python stuff
-(require 'fill-column-indicator)
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
-(setq python-shell-virtualenv-root "/home/jasn/venv/")
-
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (setq fci-rule-width 3)
-	    (setq fci-rule-color "red")
-	    (setq fci-rule-column 79)))
 
 ;; visual
 
@@ -96,6 +91,11 @@
 
 (helm-mode 1)
 
+;; jedi
+(require 'company-jedi)
+;; (setq jedi:server-args '("--virtual-env ~/venv"
+;; 			 "--virtual-env ~/webenv"))
+
 (define-key custom-bindings-map (kbd "C-c h")   'helm-command-prefix)
 (define-key custom-bindings-map (kbd "M-x")     'helm-M-x)
 (define-key custom-bindings-map (kbd "M-y")     'helm-show-kill-ring)
@@ -110,6 +110,33 @@
 (define-key helm-map (kbd "C-i")   'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-z")   'helm-select-action)
 
+;; Company
+(require 'company)
+(define-key company-mode-map (kbd "C-:") 'helm-company)
+(define-key company-active-map (kbd "C-:") 'helm-company)
+(define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "<tab>") 'company-complete)
 (define-minor-mode custom-bindings-mode
   "A mode that activates custom-bindings."
   t nil custom-bindings-map)
+
+(require 'flycheck)
+;; Python stuff
+(require 'fill-column-indicator)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+(setq python-shell-virtualenv-root "/home/jasn/venv/")
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq fci-rule-width 3)
+	    (setq fci-rule-color "red")
+	    (setq fci-rule-column 79)
+	    ;; (flycheck-mode 1)
+	    (company-mode 1)
+	    (add-to-list 'company-backends 'company-jedi)
+	    (jedi-mode 1)
+	    (company-quickhelp-mode 1)
+	    ))
